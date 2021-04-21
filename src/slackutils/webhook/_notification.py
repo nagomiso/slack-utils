644d0800cc0dea9f692f5de52547dae396ea335b
nagomiso <yuki-matsubara@c-fo.com>
2021-04-21 20:48:35 +0900

    Implemtns base code

---
+++ src/slackutils/webhook/_notification.py
@@ -0,0 +1,28 @@
+from slack_sdk.webhook import WebhookClient
+
+from data import Attachments
+
+
+def send(
+    webhook_url: str,
+    title: str,
+    workflow_id: str,
+    status: str,
+    namespace: str,
+    serviceaccount: str,
+    url: str,
+) -> None:
+    webhook = WebhookClient(url=webhook_url)
+    attachments = Attachments(
+        title=title,
+        workflow_id=workflow_id,
+        status=status,
+        namespace=namespace,
+        serviceaccount=serviceaccount,
+        url=url,
+    )
+    response = webhook.send(attachments=attachments.to_payload())
+    if response.status_code != 200:
+        raise
+    if response.body != "ok":
+        raise
